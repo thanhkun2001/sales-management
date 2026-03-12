5fa2ab8edb08e88bab89a8093b73a61202acdbd5
Thanh <nguyendinhthanh23102001@gmail.com>
2026-03-12 15:57:53 +0700

    fix frontend folder trackinxg

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,18 +1,18 @@
-import { DataSource } from 'typeorm';
-import { User } from '../entities/User';
-import dotenv from 'dotenv';
-import { Product } from '../entities/Product';
-import { Order } from '../entities/Order';
+import { DataSource } from "typeorm";
+import { User } from "../entities/User";
+import { Product } from "../entities/Product";
+import { Order } from "../entities/Order";
+import dotenv from "dotenv";
+
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'yourpassword',
-  database: process.env.DB_NAME || 'sales_db',
-  synchronize: true,   // ⚠️ chỉ dùng khi development
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  synchronize: true,
   logging: false,
-  entities: [User,Product,Order],
+  entities: [User, Product, Order],
 });
