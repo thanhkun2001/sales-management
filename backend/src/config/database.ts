7dbf52cda93d3db86ebbdd4c625ed37dc2bdc45e
Thanh <nguyendinhthanh23102001@gmail.com>
2026-03-12 16:06:17 +0700

    update

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,17 +1,19 @@
 import { DataSource } from "typeorm";
+import dotenv from "dotenv";
 import { User } from "../entities/User";
 import { Product } from "../entities/Product";
 import { Order } from "../entities/Order";
-import dotenv from "dotenv";
 
 dotenv.config();
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: isProduction
+    ? { rejectUnauthorized: false }
+    : false,
   synchronize: true,
   logging: false,
   entities: [User, Product, Order],
