aa5da542dce220bb9e08a7b867acf05f451e2984
Thanh <nguyendinhthanh23102001@gmail.com>
2026-03-13 14:14:25 +0700

    updatecc

--- frontend/src/app/core/services/order.service.ts
+++ frontend/src/app/core/services/order.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 import { CreateOrderDto, Order } from '../models/order.model';
-import { environment } from '../../../environments/environment.development';
 
 @Injectable({ providedIn: 'root' })
 export class OrderService {
