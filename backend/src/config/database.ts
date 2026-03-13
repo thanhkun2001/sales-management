7a6db8dba700dc925d5c7e3566b036b45fb75595
Thanh <nguyendinhthanh23102001@gmail.com>
2026-03-13 10:13:42 +0700

    update

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -5,7 +5,8 @@ import { Product } from "../entities/Product";
 import { Order } from "../entities/Order";
 
 dotenv.config();
-
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
